1de3afbd3b2ee13a2d111561dbe7dd738b76cf0b
Carlospasgui <carlospascualguijalba@gmail.com>
2021-05-13 12:34:38 +0200

    Piedra,papel,tjieras UML

---
+++ PiedraPapelTijera/src/MoveWordsTest.java
@@ -0,0 +1,18 @@
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class MoveWordsTest {
+
+	@Test
+	void testCheckWinner(String first,String second) {
+		int first_i, second_i;
+		first_i=4;
+		second_i=5;
+		assertEquals(first_i,second_i,1);
+		
+	}
+
+
+}
